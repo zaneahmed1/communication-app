3ebfe4d5fb2d44f994fd9eafcb9f785de06de35e
Zane Ahmed <zaneahmed4@gmail.com>
2023-02-24 18:06:38 -0500

    media queries to home page and nav

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -3,14 +3,16 @@ import "../Components/NavBar.scss"
 import home from "../home.png"
 import gear from "../gear.png"
 import logo from "../logo.png"
+import {useNavigate} from "react-router-dom"
 
 
 export default function NavBar() {
+  const navigate = useNavigate();
   return (
     <div className='navBar'>
-                <div className='navBar__logo'><img src={logo} height="100px"/></div>
+                <div className='navBar__logo'><img onClick={()=>navigate('/')} src={logo} height="100px"/></div>
         <nav>
-                <div className='navBar__listItem'><img src={home} height="70px"/></div>
+                <div className='navBar__listItem' onClick={()=>navigate('/home')}><img src={home} height="70px"/></div>
                 <div className='navBar__listItem'><img src={gear} height="70px"/></div>
       
         </nav>
