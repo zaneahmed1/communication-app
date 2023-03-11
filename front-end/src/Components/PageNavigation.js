218d68a93eda6cf8a05bab0f0260a0aa6c2b7ed7
Zane Ahmed <zaneahmed4@gmail.com>
2023-03-11 10:39:52 -0500

    adds nav functionality

--- front-end/src/Components/PageNavigation.js
+++ front-end/src/Components/PageNavigation.js
@@ -1,37 +1,81 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import home from "../home.png"
+import home from "../home.png";
 import { MenuItem } from "@mui/material";
 
+export default function PageNavigation({
+  buttons,
+  dropDownNav,
+  setDropDownNav,
+}) {
+  const navigate = useNavigate();
+  const location = useLocation();
 
-export default function PageNavigation({buttons, dropDownNav, setDropDownNav}) {
+  const copy = [...buttons];
+  const people = copy?.find((type) => {
+    if (type.button_label === "People") {
+      return type.button_image;
+    }
+  });
+  const actions = copy?.find((type) => {
+    if (type.button_label === "Actions") {
+      return type.button_image;
+    }
+  });
+  const chat = copy?.find((type) => {
+    if (type.button_label === "Chat") {
+      return type.button_image;
+    }
+  });
+  const questions = copy?.find((type) => {
+    if (type.button_label === "Questions") {
+      return type.button_image;
+    }
+  });
+  const things = copy?.find((type) => {
+    if (type.button_label === "Things") {
+      return type.button_image;
+    }
+  });
+  const timedate = copy?.find((type) => {
+    if (type.button_label === "Time/Date") {
+      return type.button_image;
+    }
+  });
 
-    const navigate = useNavigate();
-    const location = useLocation();
-
-    const copy = [...buttons]
-    const people = copy?.find((type)=>{
-        if(type.button_label === "People"){
-           return type.button_image
-       }
-    })
-    
-    const handleNav = () => {
-        setDropDownNav(!dropDownNav);
-      };
-
+  const handleNav = () => {
+    setDropDownNav(!dropDownNav);
+  };
 
   return (
     <div>
-    <div onClick={handleNav}>
-         {location.pathname === "/home" && <img src= {home} alt='home' width='200px' height='200px'/> }
-         {location.pathname === "/people" && <img src= {people.button_image} alt='people' width='200px' height='200px'/> }
-
-    </div>
-    {dropDownNav && (
-        <div><MenuItem onClick={() => navigate("/home")}>home</MenuItem> <MenuItem onClick={() => navigate("/people")}><img src={people?.button_image} width='200px'/></MenuItem> <MenuItem onClick={() => navigate("/questions")}>questions</MenuItem> <MenuItem onClick={() => navigate("/actions")}>actions</MenuItem> <MenuItem onClick={() => navigate("/things")}>things</MenuItem> <MenuItem onClick={() => navigate("/chat")}>chat</MenuItem> <MenuItem onClick={() => navigate("/timedate")}>time/date</MenuItem></div>
+      <div onClick={handleNav}>
+        {location.pathname === "/home" && (
+          <img src={home} alt="home" width="200px" height="200px" />
+        )}
+        {location.pathname === "/people" && (
+          <img
+            src={people.button_image}
+            alt="people"
+            width="200px"
+            height="200px"
+          />
+        )}
+      </div>
+      {dropDownNav && (
+        <div>
+          <MenuItem onClick={() => navigate("/home")}> <img src={home} width="200px" /></MenuItem>{" "}
+          <MenuItem onClick={() => navigate("/people")}>
+            <img src={people?.button_image} width="200px" />
+          </MenuItem>{" "}
+          <MenuItem onClick={() => navigate("/questions")}> <img src={questions?.button_image} width="200px" /></MenuItem>{" "}
+          <MenuItem onClick={() => navigate("/actions")}> <img src={actions?.button_image} width="200px" /></MenuItem>{" "}
+          <MenuItem onClick={() => navigate("/things")}> <img src={things?.button_image} width="200px" /></MenuItem>{" "}
+          <MenuItem onClick={() => navigate("/chat")}> <img src={chat?.button_image} width="200px" /></MenuItem>{" "}
+          <MenuItem onClick={() => navigate("/timedate")}> <img src={timedate?.button_image} width="200px" /></MenuItem>
+        </div>
       )}
-          </div>
-  )
+    </div>
+  );
 }
